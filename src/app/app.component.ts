205230bbf712d1ca000661992ab841a7ddb5822e
Weijing Jay Lin <weijingjaylin@gmail.com>
2024-09-27 01:04:37 -0700

    upgrade subscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,20 +29,20 @@ export class AppComponent {
   }
   fetchTodos(): void {
     const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
-    this.http.get<any[]>(apiUrl).subscribe(
-      (data) => {
+    this.http.get<any[]>(apiUrl).subscribe({
+      next: (data) => {
         this.items = data.filter((_item, idx) => idx < 10);
       },
-      (error) => {
+      error: (error) => {
         console.error('Error fetching data:', error);
-      }
-    );
+      },
+    });
   }
   isArray(obj: any) {
     return Array.isArray(obj);
   }
   addItem() {
-    this.items.push({
+    this.items.unshift({
       id: new Date().getTime().toString(),
       title: this.content,
     });
